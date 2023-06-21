7b730359a1bcf3939783a9ef0ce02f24ef7810b9
Erick Garro <erickgarro@gmail.com>
2023-06-21 17:14:33 +0200

    Added deserialization

--- components/StudentIntroList.tsx
+++ components/StudentIntroList.tsx
@@ -1,15 +1,30 @@
 import { Card } from './Card'
 import { FC, useEffect, useState } from 'react'
 import { StudentIntro } from '../models/StudentIntro'
+import * as web3 from '@solana/web3.js'
 
 const STUDENT_INTRO_PROGRAM_ID = 'HdE95RSVsdb315jfJtaykXhXY478h53X6okDupVfY9yf'
 
 export const StudentIntroList: FC = () => {
+    const connection = new web3.Connection(web3.clusterApiUrl('devnet'))
     const [studentIntros, setStudentIntros] = useState<StudentIntro[]>([])
 
     useEffect(() => {
-        setStudentIntros(StudentIntro.mocks)
-    }, [])
+        const fetchStudentIntros = async () => {
+            const accounts = await connection.getProgramAccounts(new web3.PublicKey(STUDENT_INTRO_PROGRAM_ID));
+            
+            const studentIntros: StudentIntro[] = accounts.reduce((accum: StudentIntro[], { pubkey, account }) => {
+                const studentIntro = StudentIntro.deserialize(account.data);
+                if(!studentIntro) return accum;
+
+                return [...accum, studentIntro];
+            }, []);
+    
+            setStudentIntros(studentIntros);
+        }
+    
+        fetchStudentIntros();
+    }, []);
     
     return (
         <div>
--- models/StudentIntro.ts
+++ models/StudentIntro.ts
@@ -9,14 +9,14 @@ export class StudentIntro {
         this.message = message;
     }
 
-    static mocks: StudentIntro[] = [
-        new StudentIntro('Elizabeth Holmes', `Learning Solana so I can use it to build sick NFT projects.`),
-        new StudentIntro('Jack Nicholson', `I want to overhaul the world's financial system. Lower friction payments/transfer, lower fees, faster payouts, better collateralization for loans, etc.`),
-        new StudentIntro('Terminator', `i'm basically here to protect`),
-    ]
-
-    borshInstructionSchema = borsh.struct([
+   borshInstructionSchema = borsh.struct([
         borsh.u8('variant'),
+        borsh.str('name'),
+        borsh.str('message'),
+    ])
+
+    static borshInstructionSchema = borsh.struct([
+        borsh.u8('initialized'),
         borsh.str('name'),
         borsh.str('message'),
     ])
@@ -25,5 +25,17 @@ export class StudentIntro {
         const buffer = Buffer.alloc(1000)
         this.borshInstructionSchema.encode({ ...this, variant: 0 }, buffer)
         return buffer.slice(0, this.borshInstructionSchema.getSpan(buffer))
+    }
+
+    static deserialize(buffer?: Buffer): StudentIntro|null {
+        if (!buffer) return null;
+
+        try {
+            const { name, message } = this.borshInstructionSchema.decode(buffer);
+            return new StudentIntro(name, message);
+        } catch(e) {
+            console.log('Deserialization error:', e)
+            return null;
+        }
     }
 }
